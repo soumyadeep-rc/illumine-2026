d8672e52e732640e5d38f5daabb982373580adb1
rajdeepcodeshere247 <rajdeepdasyear2006@gmail.com>
2026-04-28 10:47:39 +0530

    About page first version

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import Hero from "./sections/Hero";
+import AboutIllumine from "../components/about/AboutIllumine";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex flex-col">
       <Hero />
+      <AboutIllumine />
     </div>
   );
 }

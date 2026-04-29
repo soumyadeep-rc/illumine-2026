261d748dec96fe93cb0483b689d9ce153cc0575a
rajdeepcodeshere247 <rajdeepdasyear2006@gmail.com>
2026-04-29 10:56:45 +0530

    structure app/page.tsx and fixed routesand about section and improvised the build and folder structure for production

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
 import Hero from "./sections/Hero";
+import AboutIllumine from "../components/about/AboutIllumine";
 
 /**
  * MODULE: Home Route (/)
- * 
- * DESCRIPTION:
+ * * DESCRIPTION:
  * This is the root page for the Illumine 2026 application. 
  */
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <Hero />
+      <AboutIllumine />
     </div>
   );
 }
---
+++ data/aboutilluminepaths/index.ts
@@ -0,0 +1,10 @@
+export const svgPaths = {
+  aboutIllumine: "/svg/About-IlluMine.svg",
+  upperBorder: "/svg/upper-border.svg",
+  lowerBorder: "/svg/lower-border.svg",
+  group101: "/svg/Group 101.svg",
+  vector: "/svg/Vector.svg",
+  frame29: "/svg/Frame 29.svg",
+  frame30: "/svg/Frame 30.svg",
+  group97: "/svg/Group 97.svg",
+};
